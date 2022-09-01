7794d8ab44c1004acc6bf1ec77533144a983a561
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-09-01 18:54:30 +0900

    add: ts type asseertions

---
+++ Practice/typescript/typeAssertions.ts
@@ -0,0 +1,11 @@
+// <>
+
+let myName: any = "혜수"
+let myNameLength: number = (<string>myName).length
+
+console.log(myNameLength)
+
+// as
+
+myName = "하잉zzzzz"
+console.log((myName as string).length)
