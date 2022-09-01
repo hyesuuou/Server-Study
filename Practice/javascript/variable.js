04997aec8fae42ddb768a497c4bef4d5748ab57c
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-09-01 17:07:33 +0900

    add: js var, let, const

---
+++ Practice/javascript/variable.js
@@ -0,0 +1,22 @@
+// var, let, const
+
+// var 변수인데 또 선언도 됨
+
+var a = "하이"
+var a = "이게또됨?"
+
+console.log(a)
+
+// let 변수
+
+let b = "ㅋㅋ"
+b = "ㅎㅎ"
+
+console.log(b)
+
+// const 상수 -> 수 바꾸는거 안됨
+
+const c = "상수"
+//c = "바뀜?" -> error
+
+console.log(c)
